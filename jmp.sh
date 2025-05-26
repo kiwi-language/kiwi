c46e8d3c45b8d1dc5ace8326596596dbe5678537
wizardleen <wizardleen@gmail.com>
2025-05-26 22:29:29 +0800

    chore: create scripts to reset development server

---
+++ jmp.sh
@@ -0,0 +1,4 @@
+ssh root@metavm.tech "sh ~/develop/kiwi/bin/kiwi-server stop"
+ssh -L 7001:localhost:5432 root@metavm.tech -f -N -T
+ssh -L 7002:localhost:9200 root@metavm.tech -f -N -T
+ssh -L 7003:localhost:8080 root@metavm.tech -f -N -T
