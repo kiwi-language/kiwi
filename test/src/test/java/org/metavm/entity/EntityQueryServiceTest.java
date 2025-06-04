f0c97d589be8e20754f9333a1a0a56e649f39d0a
wizardleen <wizardleen@gmail.com>
2025-06-04 22:01:31 +0800

    doc: Document application retrieval API fix(server): Resolve entity query pagination issue

--- test/src/test/java/org/metavm/entity/EntityQueryServiceTest.java
+++ test/src/test/java/org/metavm/entity/EntityQueryServiceTest.java
@@ -2,11 +2,14 @@ package org.metavm.entity;
 
 import junit.framework.TestCase;
 import org.junit.Assert;
+import org.metavm.application.Application;
 import org.metavm.common.Page;
+import org.metavm.compiler.util.List;
 import org.metavm.mocks.Foo;
 import org.metavm.mocks.Qux;
 import org.metavm.object.instance.core.IInstanceContext;
 import org.metavm.object.instance.core.Id;
+import org.metavm.user.PlatformUser;
 import org.metavm.util.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -90,5 +93,50 @@ public class EntityQueryServiceTest extends TestCase {
             Assert.assertSame(foo, page.items().getFirst());
         }
     }
+
+    public void testPagesize() {
+        var appId = TestUtils.doInTransaction(() -> {
+            try (var context = entityContextFactory.newContext(Constants.PLATFORM_APP_ID)) {
+                var user = new PlatformUser(context.allocateRootId(), "test", "123456", "test", List.of());
+                context.bind(user);
+                context.bind(new Application(
+                        context.allocateRootId(),
+                        "app1",
+                        user
+                ));
+
+                context.bind(new Application(
+                        context.allocateRootId(),
+                        "app2",
+                        user
+                ));
+
+                Application app;
+                context.bind(app = new Application(
+                        context.allocateRootId(),
+                        "app3",
+                        user
+                ));
+                context.finish();
+                return app.getId();
+            }
+        });
+        TestUtils.waitForEsSync(schedulerAndWorker);
+
+        try (var context = entityContextFactory.newContext(Constants.PLATFORM_APP_ID)) {
+            ContextUtil.setAppId(Constants.PLATFORM_APP_ID);
+            var app = (Application) context.get(appId);
+            assertEquals("app3", app.getName());
+            var page = entityQueryService.query(EntityQueryBuilder.newBuilder(Application.class)
+                    .page(1)
+                    .pageSize(2)
+                    .build(),
+                    context
+            );
+            assertEquals(2, page.items().size());
+            logger.debug("First app name: {}", page.items().getFirst().getName());
+        }
+    }
+
 
 }
