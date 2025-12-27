e8def6e56a00a2a0869b57a51f7711e0f5ff08cb
wizardleen <wizardleen@gmail.com>
2025-12-27 15:17:26 +0800

    Increase default session timeout to 10S

--- test/src/test/java/org/metavm/Lab.java
+++ test/src/test/java/org/metavm/Lab.java
@@ -1,13 +1,21 @@
 package org.metavm;
 
 
+import lombok.SneakyThrows;
 import org.metavm.util.Utils;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
 
 public class Lab {
 
+    @SneakyThrows
     public static void main(String[] args) {
 
-        Utils.clearDirectory("/Users/leen/workspace/kiwi/test/target/test-classes/target");
+        Files.writeString(Path.of("/tmp/lab/test.kiwi"), "class Product(\r\n" +
+                "                var name: string\r\n" +
+                "                var price: double\r\n" +
+                "            )\r\n");
 
     }
 
